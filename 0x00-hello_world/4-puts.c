0d447330e4f68436ffa050096261f2d0010d5519
Daniel-Mojere1 <mojereabioladaniel@gmail.com>
2023-03-17 03:38:30 -0700

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-int main(void)
+/**
+ * main: my main function
+ *
+ * Description: Hello World
+ */
 {
-	/*
-	 * main input
-	 * return 0 always
-	 */
 	puts("Programming is like building a multilingual puzzle \n");
 	return(0);
 }
